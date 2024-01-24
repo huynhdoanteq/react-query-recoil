5707cdc41443a401d22ab710b6f330fb564f8d87
huynhdoanteq <huynhdoan@teqnological.asia>
2024-01-24 10:01:47 +0700

    add social login

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,58 @@
-import { RouterProvider } from "react-router-dom";
 import "./App.css";
+import type { Session } from "@supabase/supabase-js";
+import { createClient } from "@supabase/supabase-js";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { useEffect, useState } from "react";
 import { router } from "@src/router";
+import { RouterProvider } from "react-router-dom";
+
+const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_PROJECT_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+);
 
 function App() {
-  return <RouterProvider router={router} />;
+  const [sessions, setSessions] = useState<undefined | null | Session>(
+    undefined
+  );
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSessions(session);
+    });
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSessions(session);
+    });
+    return () => subscription.unsubscribe();
+  }, []);
+
+  console.log(sessions);
+  if (!sessions) {
+    return (
+      <div className="max-w-[500px] m-auto">
+        <Auth
+          supabaseClient={supabase}
+          appearance={{ theme: ThemeSupa }}
+          providers={[
+            "google",
+            "discord",
+            "apple",
+            "figma",
+            "github",
+            "linkedin_oidc",
+          ]}
+          providerScopes={{
+            google: "https://www.googleapis.com/auth/drive.readonly",
+          }}
+        />
+      </div>
+    );
+  } else {
+    return <RouterProvider router={router} />;
+  }
 }
 
 export default App;
